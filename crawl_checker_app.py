45249cf0b68b2c1d13ad5d86da6390a0aeca9b06
Moataz samy <mottaz@gmail.com>
2025-04-20 18:37:14 +0200

    change to allow reppy again

--- crawl_checker_app.py
+++ crawl_checker_app.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 import pandas as pd
 import re
 from concurrent.futures import ThreadPoolExecutor, as_completed
-#from reppy.robots import Robots
+from reppy.robots import Robots
 # Try importing html escape depending on Python version
 try:
     import html
@@ -82,15 +82,14 @@ def check_site(url, user_agent='*', timeout=5):
         rss_found = "rss" in content.lower() or "feed" in content.lower()
         js_heavy = any(k in content.lower() for k in ["webpack", "window.__INITIAL_STATE__", "react", "vue", "next.js"])
 
-        # ❌ Removed reppy.robots to make the app work on Streamlit Cloud
-        # try:
-        #     reppy_data = Robots.fetch(robots_url)
-        #     if not sitemap_matches and reppy_data.sitemaps:
-        #         sitemap_info = ', '.join(reppy_data.sitemaps)
-        #     if not crawl_allowed and reppy_data.allowed(url, user_agent):
-        #         crawl_allowed = True
-        # except:
-        #     pass
+        try:
+            reppy_data = Robots.fetch(robots_url)
+            if not sitemap_matches and reppy_data.sitemaps:
+                sitemap_info = ', '.join(reppy_data.sitemaps)
+            if not crawl_allowed and reppy_data.allowed(url, user_agent):
+                crawl_allowed = True
+        except:
+            pass
 
         api_url = get_known_api(domain)
 
